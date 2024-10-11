b159461cf32da781c0b5a60cc47ffaea7f8e1610
pimphand <faisaldwiki69@gmail.com>
2024-10-11 11:06:27 +0700

    add rekap register

--- app/Http/Controllers/RecapController.php
+++ app/Http/Controllers/RecapController.php
@@ -12,6 +12,7 @@ use App\Models\Rectal;
 use App\Models\Spirometri;
 use App\Models\TandaVital;
 use App\Services\ParticipantService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -50,14 +51,26 @@ class RecapController extends Controller
             $data = Participant::select(
                 DB::raw('DATE(participants.register_date) as register_date'),
                 DB::raw('COUNT(DISTINCT participants.id) as total_participants'), // Count distinct participants
-                DB::raw('SUM(CASE WHEN audiometris.selesai = 1 THEN 1 ELSE 0 END) as total_audiometris'),
-                DB::raw('SUM(CASE WHEN pemeriksaan_fisiks.selesai = 1 THEN 1 ELSE 0 END) as total_pemeriksaan_fisiks'),
-                DB::raw('SUM(CASE WHEN laboratoria.selesai = 1 THEN 1 ELSE 0 END) as total_laboratoria'),
-                DB::raw('SUM(CASE WHEN radiologis.selesai = 1 THEN 1 ELSE 0 END) as total_radiologis'),
-                DB::raw('SUM(CASE WHEN rectals.selesai = 1 THEN 1 ELSE 0 END) as total_rectals'),
-                DB::raw('SUM(CASE WHEN spirometris.selesai = 1 THEN 1 ELSE 0 END) as total_spirometris'),
-                DB::raw('SUM(CASE WHEN tanda_vitals.selesai = 1 THEN 1 ELSE 0 END) as total_tanda_vitals'),
-                DB::raw('SUM(CASE WHEN ekgs.selesai = 1 THEN 1 ELSE 0 END) as total_ekgs')
+                DB::raw('COUNT(audiometris.id) as total_audiometris'), // Count total audiometris records
+                DB::raw('SUM(CASE WHEN audiometris.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_audiometris'),
+                DB::raw('COUNT(pemeriksaan_fisiks.id) as total_pemeriksaan_fisiks'), // Count total pemeriksaan fisiks records
+                DB::raw('SUM(CASE WHEN pemeriksaan_fisiks.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_pemeriksaan_fisiks'),
+                DB::raw('SUM(CASE WHEN pemeriksaan_fisiks.kesimpulan = "FIT" THEN 1 ELSE 0 END) as total_selesai_pemeriksaan_fit'),
+                DB::raw('SUM(CASE WHEN pemeriksaan_fisiks.kesimpulan = "FIT WITH RETRICTION" THEN 1 ELSE 0 END) as total_selesai_pemeriksaan_frw'),
+                DB::raw('SUM(CASE WHEN pemeriksaan_fisiks.kesimpulan = "UNFIT" THEN 1 ELSE 0 END) as total_selesai_pemeriksaan_unfit'),
+                DB::raw('COUNT(laboratoria.id) as total_laboratoria'), // Count total laboratoria records
+                DB::raw('SUM(CASE WHEN laboratoria.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_laboratoria'),
+                DB::raw('COUNT(radiologis.id) as total_radiologis'), // Count total radiologis records
+                DB::raw('SUM(CASE WHEN radiologis.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_radiologis'),
+                DB::raw('COUNT(rectals.id) as total_rectals'), // Count total rectals records
+                DB::raw('SUM(CASE WHEN rectals.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_rectals'),
+                DB::raw('COUNT(spirometris.id) as total_spirometris'), // Count total spirometris records
+                DB::raw('SUM(CASE WHEN spirometris.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_spirometris'),
+                DB::raw('COUNT(tanda_vitals.id) as total_tanda_vitals'), // Count total tanda vitals records
+                DB::raw('SUM(CASE WHEN tanda_vitals.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_tanda_vitals'),
+                DB::raw('SUM(CASE WHEN tanda_vitals.ibu_hamil = 1 THEN 1 ELSE 0 END) as total_hamil'),
+                DB::raw('COUNT(ekgs.id) as total_ekgs'), // Count total ekgs records
+                DB::raw('SUM(CASE WHEN ekgs.selesai = 1 THEN 1 ELSE 0 END) as total_selesai_ekgs')
             )
                 ->leftJoin('audiometris', 'participants.id', '=', 'audiometris.participant_id')
                 ->leftJoin('pemeriksaan_fisiks', 'participants.id', '=', 'pemeriksaan_fisiks.participant_id')
@@ -68,7 +81,34 @@ class RecapController extends Controller
                 ->leftJoin('tanda_vitals', 'participants.id', '=', 'tanda_vitals.participant_id')
                 ->leftJoin('ekgs', 'participants.id', '=', 'ekgs.participant_id')
                 ->groupBy(DB::raw('DATE(participants.register_date)'))
+                ->where(function ($query)use ($request) {
+                    $date = explode(' to ', $request->date_range);
+                    // dd(count($date));
+                    // Jika hanya ada satu tanggal
+                    if (count($date) == 1) {
+                        $startDate = Carbon::parse($date[0])->startOfDay();
+                        $endDate = Carbon::parse($date[0])->endOfDay();
+                    } else {
+                        // Jika ada dua tanggal
+                        $startDate = Carbon::parse($date[0])->startOfDay();
+                        $endDate = Carbon::parse($date[1])->endOfDay();
+                    }
+
+                    $query->whereBetween('register_date', [$startDate, $endDate]);
+                    if ($request->contract_id){
+                        $query->where('contract_id',$request->contract_id);
+                    }
+                    if ($request->divisi_id){
+                        $query->where('divisi_id',$request->divisi_id);
+                    }
+                    if ($request->client_id){
+                        $query->where('client_id',$request->client_id);
+                    }
+
+
+                })
                 ->get();
+
             return $data;
         }
         return view('pages.recaps.register', compact('data', 'client'));
--- resources/views/pages/recaps/register.blade.php
+++ resources/views/pages/recaps/register.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.main')
 
-@section('title', 'Divisi')
-
+@section('title', 'Rekap Register')
 @section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/pickers/pickadate/pickadate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
-
 @section('content')
     <style>
         .table-responsive table.textkecil {
-            font-size: 10px !important; /* Atur ukuran font kecil dan prioritaskan */
+            font-size: 12px !important; /* Atur ukuran font kecil dan prioritaskan */
         }
 
         .table-responsive th.textkecil, .table-responsive td.textkecil {
-            font-size: 10px !important; /* Pastikan ukuran font pada header dan sel tabel */
+            font-size: 12px !important; /* Pastikan ukuran font pada header dan sel tabel */
         }
 
         .table thead th {
@@ -27,10 +28,48 @@
     <!-- Basic Tabs starts -->
     <div class="card">
         <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3">
+                        <label class="form-label" for="fp-range">Tanggal</label>
+                        <input type="text" name="date_range" id="fp-range" class="form-control flatpickr-range"
+                               placeholder="YYYY-MM-DD to YYYY-MM-DD" />
+                    </div>
+                    <div class="col-md-3">
+                        <label for="" class="form-label">Client</label>
+                        <select name="client_id" id="client_id" class="form-select">
+                            <option value="{{ $client->id }}">{{ $client->name }}</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="" class="form-label">Kontrak</label>
+                        <select name="contract_id" id="contract_id" class="form-select">
+                            <option value="">Pilih</option>
+                            @foreach ($client->contracts as $item)
+                                <option value="{{ $item->id }}"
+                                    {{ session('contract_id') == $item->id ? 'selected' : '' }}>{{ $item->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="" class="form-label">Divisi</label>
+                        <select name="divisi_id" id="divisi_id" class="form-select">
+                            <option value="">Pilih</option>
+                            @foreach ($client->divisi as $item)
+                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-md-1">
+                        <label for="" class="form-label text-white">cari</label>
+                        <button type="button" class="btn btn-primary" id="cari">Cari</button>
+                    </div>
+                </div>
+        </div>
+        <div class="card-body">
             <div class="table-responsive text-nowrap">
                 <table class="dt-responsive table mt-1 textkecil" id="table">
                     <thead>
-                        <tr class="textkecil">
+                        <tr class="textkecil" style="font-weight:500">
                             <th rowspan="2" style="font-family:bold;color:#000;border-color:#000" class="border-bottom textkecil border-top border-left border-right text-center align-middle">Tgl</th>
                             <th rowspan="2" style="font-family:bold;color:#000;border-color:#000" class="border-bottom textkecil border-top border-left border-right text-center align-middle">Jml Peserta</th>
                             <th colspan="10" style="font-family:bold;color:#000;border-color:#000" class="border-bottom textkecil border-top border-left border-right text-center">U</th>
@@ -62,32 +101,11 @@
                         </tr>
                     </thead>
 
-                    <tbody>
-                        <tr align="center">
-                            <td class="border-bottom border-top text-center">12-08-2024</td>
-                                  <td class="border-bottom border-top text-center">108</td>
-                                  <td class="border-bottom border-top text-center">108</td>
-                                                                      <td class="border-bottom border-top text-center text-success">59</td>
-                                  <td class="border-bottom border-top text-center text-success">49</td>
-                                  <td class="border-bottom border-top text-center text-success">0</td>
+                    <tbody id="data_table" style="font-weight:400">
 
-                                  <td class="border-bottom border-top text-center text-success">108</td>
-                                  <td class="border-bottom border-top text-center text-success">108</td>
-                                  <td class="border-bottom border-top text-center text-success">108</td>
-                                  <td class="border-bottom border-top text-center text-success">107</td>
-                                  <td class="border-bottom border-top text-center text-success">0</td>
-                                  <td class="border-bottom border-top text-center text-success">0</td>
+                    </tbody>
+                    <tbody id="data_total">
 
-                                  <td class="border-bottom border-top text-center">59</td>
-                                  <td class="border-bottom border-top text-center text-success">59</td>
-                                  <td class="border-bottom border-top text-center">37</td>
-                                  <td class="border-bottom border-top text-center text-success">37</td>
-                                  <td class="border-bottom border-top text-center">82</td>
-                                  <td class="border-bottom border-top text-center text-success">82</td>
-                                  <td class="border-bottom border-top text-center">0</td>
-                                  <td class="border-bottom border-top text-center text-success">0</td>
-                                  <td class="border-bottom border-top text-center">1</td>
-                              </tr>
                     </tbody>
                 </table>
             </div>
@@ -98,15 +116,153 @@
 @endsection
 
 @section('js')
-    <script>
-        $(function() {
-            loadDataTable();
-            function loadDataTable() {
-                $.get('{{ route('recap.register') }}', function(response) {
-                    console.log(response)
-                })
-            }
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.date.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.time.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/legacy.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
 
-        })
+    <script>
+        $(document).ready(function() {
+            $('.flatpickr-range').flatpickr({
+                mode: 'range',
+                maxDate: 'today',
+
+            });
+            $('#cari').click(function (e){
+                loadDataTable();
+            })
+
+            function loadDataTable() {
+                let url = "{{ route('recap.register') }}"
+
+                // Get the selected date range
+                let dateRange = $('#fp-range').val();
+
+                // Append date range to the URL if it's selected
+                if (dateRange) {
+                    url += `?date_range=${dateRange}`;
+                }
+
+                // Append client, contract, and division filters to the URL
+                let clientId = $('#client_id').val();
+                let contractId = $('#contract_id').val();
+                let divisiId = $('#divisi_id').val();
+
+                if (clientId) {
+                    url += `&client_id=${clientId}`;
+                }
+                if (contractId) {
+                    url += `&contract_id=${contractId}`;
+                }
+                if (divisiId) {
+                    url += `&divisi_id=${divisiId}`;
+                }
+                $.get(url, function(response) {
+                    $("#data_table").html(''); // Clear the table content
+                    let totalParticipants = 0;
+                    let totalSelesaiPemeriksaanFit = 0;
+                    let totalSelesaiPemeriksaanFrw = 0;
+                    let totalSelesaiPemeriksaanUnfit = 0;
+                    let totalTandaVitals = 0;
+                    let totalPemeriksaanFisiks = 0;
+                    let totalLaboratoria = 0;
+                    let totalRadiologis = 0;
+                    let totalField10 = 0;
+                    let totalField11 = 0;
+                    let totalAudiometris = 0;
+                    let totalSelesaiAudiometris = 0;
+                    let totalEkgs = 0;
+                    let totalSelesaiEkgs = 0;
+                    let totalSpirometris = 0;
+                    let totalSelesaiSpirometris = 0;
+                    let totalRectals = 0;
+                    let totalSelesaiRectals = 0;
+                    let totalHamil = 0;
+                    // Iterate through the response using $.each
+                    $.each(response, function(index, data) {
+                        totalParticipants += parseInt(data.total_participants);
+                        totalSelesaiPemeriksaanFit += parseInt(data.total_selesai_pemeriksaan_fit);
+                        totalSelesaiPemeriksaanFrw += parseInt(data.total_selesai_pemeriksaan_frw);
+                        totalSelesaiPemeriksaanUnfit += parseInt(data.total_selesai_pemeriksaan_unfit);
+                        totalTandaVitals += parseInt(data.total_tanda_vitals);
+                        totalPemeriksaanFisiks += parseInt(data.total_pemeriksaan_fisiks);
+                        totalLaboratoria += parseInt(data.total_laboratoria);
+                        totalRadiologis += parseInt(data.total_radiologis);
+                        totalField10 += parseInt(data.field10);
+                        totalField11 += parseInt(data.field11);
+                        totalAudiometris += parseInt(data.total_audiometris);
+                        totalSelesaiAudiometris += parseInt(data.total_selesai_audiometris);
+                        totalEkgs += parseInt(data.total_ekgs);
+                        totalSelesaiEkgs += parseInt(data.total_selesai_ekgs);
+                        totalSpirometris += parseInt(data.total_spirometris);
+                        totalSelesaiSpirometris += parseInt(data.total_selesai_spirometris);
+                        totalRectals += parseInt(data.total_rectals);
+                        totalSelesaiRectals += parseInt(data.total_selesai_rectals);
+                        totalHamil += parseInt(data.total_hamil);
+                        // Construct the row content dynamically
+                        let content = `
+                            <tr align="center">
+                                <td class="border-bottom border-top text-start">${data.register_date}</td>
+                                <td class="border-bottom border-top text-center">${data.total_participants}</td>
+                                <td class="border-bottom border-top text-center">${data.total_participants}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_pemeriksaan_fit}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_pemeriksaan_frw}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_pemeriksaan_unfit}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_tanda_vitals	}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_pemeriksaan_fisiks	}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_laboratoria}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_radiologis}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.field10}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.field11}</td>
+                                <td class="border-bottom border-top text-center">${data.total_audiometris}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_audiometris}</td>
+                                <td class="border-bottom border-top text-center">${data.total_ekgs}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_ekgs	}</td>
+                                <td class="border-bottom border-top text-center">${data.total_spirometris}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_spirometris	}</td>
+                                <td class="border-bottom border-top text-center">${data.total_rectals}</td>
+                                <td class="border-bottom border-top text-center text-success">${data.total_selesai_rectals}</td>
+                                <td class="border-bottom border-top text-center">${data.total_hamil}</td>
+                            </tr>
+                            `;
+
+                        // Append the constructed content to the table
+                        $("#data_table").append(content);
+                    });
+
+                    // After looping through all records, append the total row
+                    let totalContent = `
+                        <tr align="center">
+                            <td class="border-bottom border-top text-start">Total</td>
+                            <td class="border-bottom border-top text-center">${totalParticipants}</td>
+                            <td class="border-bottom border-top text-center">${totalParticipants}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiPemeriksaanFit}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiPemeriksaanFrw}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiPemeriksaanUnfit}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalTandaVitals}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalPemeriksaanFisiks}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalLaboratoria}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalRadiologis}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalField10}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalField11}</td>
+                            <td class="border-bottom border-top text-center">${totalAudiometris}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiAudiometris}</td>
+                            <td class="border-bottom border-top text-center">${totalEkgs}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiEkgs}</td>
+                            <td class="border-bottom border-top text-center">${totalSpirometris}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiSpirometris}</td>
+                            <td class="border-bottom border-top text-center">${totalRectals}</td>
+                            <td class="border-bottom border-top text-center text-success">${totalSelesaiRectals}</td>
+                            <td class="border-bottom border-top text-center">${totalHamil}</td>
+                        </tr>
+                    `;
+
+                    // Append the total row to the table
+                    $("#data_table").append(totalContent);
+                });
+            }
+        });
     </script>
+
 @endsection
