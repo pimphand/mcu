e359ff9cc740ae2a4599d2277640971a43a19a28
pimphand <faisaldwiki69@gmail.com>
2025-04-22 14:58:24 +0700

    fixing filter

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -483,7 +483,7 @@ class ParticipantController extends Controller
                     AllowedFilter::exact('divisi_id'),
                     AllowedFilter::scope('date_range'),
                 ])
-                ->defaultSort('no_form')
+                ->defaultSort('-created_at')
                 ->get();
         }
         return view('pages.participant.print-mcu', compact('client', 'data'));
