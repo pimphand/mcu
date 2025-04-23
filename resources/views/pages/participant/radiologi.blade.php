25ebfe8da622a1780e87a3be0f8b8083ff41adbf
pimphand <faisaldwiki69@gmail.com>
2025-04-23 16:09:14 +0700

    fixing radiologi

--- resources/views/pages/participant/radiologi.blade.php
+++ resources/views/pages/participant/radiologi.blade.php
@@ -3,7 +3,8 @@
 @endphp
 <div class="modal-header">
     <h5 class="modal-title" id="modal-edit-detail-title">
-        {{ 'Radiologi MCU ID ' . $participant->code . ' | ' . $participant->name }}</h5>
+        {{ 'Radiologi MCU ID ' . $participant->code . ' | ' . $participant->name }}
+    </h5>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 <div class="modal-body">
@@ -19,26 +20,29 @@
         <div class="col-md-6 mb-2">
             <div class="form-group">
                 <label for="" class="form-label">Diafragma Sinus</label>
-                <textarea name="diafragma_sinus" id="diafragma_sinus" cols="30" rows="2" class="form-control">{{ $radiologi?->diafragma_sinus }}</textarea>
+                <textarea name="diafragma_sinus" id="diafragma_sinus" cols="30" rows="2"
+                    class="form-control">{{ $radiologi?->diafragma_sinus }}</textarea>
             </div>
         </div>
         <div class="col-md-6 mb-2">
             <div class="form-group">
                 <label for="" class="form-label">Pulmo</label>
-                <textarea name="pulmo" id="pulmo" cols="30" rows="2" class="form-control">{{ $radiologi?->pulmo }}</textarea>
+                <textarea name="pulmo" id="pulmo" cols="30" rows="2"
+                    class="form-control">{{ $radiologi?->pulmo }}</textarea>
             </div>
         </div>
         <div class="col-md-6 mb-2">
             <div class="form-group">
                 <label for="" class="form-label">Kesan</label>
-                <textarea name="kesan" id="kesan" cols="30" rows="2" class="form-control">{{ $radiologi?->kesan }}</textarea>
+                <textarea name="kesan" id="kesan" cols="30" rows="2"
+                    class="form-control">{{ $radiologi?->kesan }}</textarea>
             </div>
         </div>
         <div class="col-md-4 mb-2">
             <div class="form-group">
                 <label class="form-label required" for="">Petugas Pemeriksa</label>
-                <select name="employee_id"  class="form-control employee_id" required>
-                    @foreach ($employees as  $employee)
+                <select name="employee_id" class="form-control employee_id" required>
+                    @foreach ($employees as $employee)
                         <option value="{{ $employee->id }}" {{$employee->id == $radiologi?->employee?->id ? "selected" : ''  }}>{{ $employee->nama }}</option>
                     @endforeach
 
@@ -59,14 +63,19 @@
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-danger _btn_danger" data-bs-dismiss="modal">Batal</button>
-    <button type="button" class="btn btn-info" onclick="window.open('{{ route('report.radiologi', $participant->id) }}', '', 'toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=600');">Print</button>
+    <button type="button" class="btn btn-info"
+        onclick="window.open('{{ route('report.radiologi', $participant->id) }}', '', 'toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=600');">Print</button>
     <button type="submit" class="btn btn-primary" id="submit-edit-detail">Submit</button>
 </div>
 
 
 <script>
-  var selectElement = document.querySelector('.employee_id');
-    selectElement.value = "3";
+    var selectElement = document.querySelector('.employee_id');
+    @if($radiologi?->employee?->id)
+        selectElement.value = "{{ $radiologi?->employee?->id }}";
+    @else
+        selectElement.value = "5";
+    @endif
     // Jika perlu memicu event 'change'
     var event = new Event('change');
     selectElement.dispatchEvent(event);
